6e1657389c901352e467c940eaf68364da30401b
jiangsichengchn <fashedeshujidan@outlook.com>
2023-04-27 21:47:28 +0800

    铭刻太吾

---
+++ InscribeTaiwu/InscribeTaiwu.cs
@@ -0,0 +1,41 @@
+﻿using HarmonyLib;
+using TaiwuModdingLib.Core.Plugin;
+using UnityEngine;
+
+namespace InscribeTaiwu
+{
+    [PluginConfig("InscribeTaiwu", "发射的熟鸡蛋", "1.0")]
+    public class InscribeTaiwu : TaiwuRemakePlugin
+    {
+        private Harmony harmony;
+
+        public override void Dispose()
+        {
+            harmony.UnpatchSelf();
+        }
+
+        public override void Initialize()
+        {
+            harmony = Harmony.CreateAndPatchAll(typeof(InscribeTaiwu));
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(UI_CharacterMenuInfo), "OnCurrentCharacterChange")]
+        public static void OnCurrentCharacterChange_Postfix(UI_CharacterMenuInfo __instance)
+        {
+            int taiwuCharId = SingletonObject.getInstance<BasicGameData>().TaiwuCharId;
+            bool isTaiwu = __instance.CharacterMenu.CurCharacterId == taiwuCharId;
+            if (isTaiwu)
+            {
+                CButton inscribeBtn = __instance.CGet<CButton>("InscribeBtn");
+                inscribeBtn.gameObject.SetActive(true);
+                inscribeBtn.interactable = true;
+
+                Refers btnRefers = inscribeBtn.GetComponent<Refers>();
+                btnRefers.CGet<GameObject>("Label").SetActive(inscribeBtn.interactable);
+                btnRefers.CGet<GameObject>("LabelDisable").SetActive(!inscribeBtn.interactable);
+                btnRefers.CGet<CImage>("Icon").SetSprite(inscribeBtn.interactable ? "charactermenu3_01_gn_icon_1_0" : "charactermenu3_01_gn_icon_1_1");
+            }
+        }
+    }
+}
---
+++ InscribeTaiwuBackend/InscribeTaiwuBackend.cs
@@ -0,0 +1,38 @@
+﻿using TaiwuModdingLib.Core.Plugin;
+using HarmonyLib;
+using GameData.Domains.Character;
+using GameData.Domains;
+using GameData.Domains.Global.Inscription;
+using GameData.Utilities;
+
+namespace InscribeTaiwuBackend
+{
+    [PluginConfig("InscribeTaiwuBackend", "发射的熟鸡蛋", "1.0")]
+    public class InscribeTaiwuBackend : TaiwuRemakePlugin
+    {
+        private Harmony harmony;
+
+        public override void Dispose()
+        {
+            harmony.UnpatchSelf();
+        }
+
+        public override void Initialize()
+        {
+            harmony = Harmony.CreateAndPatchAll(typeof(InscribeTaiwuBackend));
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(CharacterDomain), "GetInscriptionStatus")]
+        public static void GetInscriptionStatus_Postfix(ref sbyte __result, int charId)
+        {
+            int taiwuCharId = DomainManager.Taiwu.GetTaiwuCharId();
+            if (charId == taiwuCharId)
+            {
+                uint worldId = DomainManager.World.GetWorldId();
+                InscribedCharacterKey key = new InscribedCharacterKey(worldId, charId);
+                __result = (sbyte)((!DomainManager.Global.IsCharacterInscribed(key)) ? 1 : 2);
+            }
+        }
+    }
+}
